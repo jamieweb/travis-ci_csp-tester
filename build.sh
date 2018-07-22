0b5516d53b2003d506e9d33bb4a31af68c00c671
Jamie Scaife <10353998+jamieweb@users.noreply.github.com>
2018-07-22 23:04:20 +0100

    Added sleep to prevent segfault, added conditional output

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 node crawl.js
-printf "\nCrawled Pages:\n$(cat crawled.csv)\n\nContent Security Policy Reports:\n$(cat csp-reports.txt)"
+sleep 1 #This sleep is required otherwise PHP will segfault when it is terminated at the end of the build.
+printf "\nCrawled Pages:\n$(cat crawled.csv)\n\n"
+if [ -s csp-reports.txt ]; then printf "Content Security Policy Reports:\n$(cat csp-reports.txt)"; else printf "No Reports Generated! Success!"; fi
