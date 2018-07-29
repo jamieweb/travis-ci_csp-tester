41a31fda588d4ef1b37ecc142d2ef7bfb1f65985
Jamie Scaife <10353998+jamieweb@users.noreply.github.com>
2018-07-30 00:38:42 +0100

    Added link to example dir

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <h1>Test page to generate some example reports.</h1>
+<a href="/example-directory/">Example Directory</a>
 <img src="/test-image.png">
 <script src="/test-script.js"></script>
 <span style="color: green">Inline Style</span>
