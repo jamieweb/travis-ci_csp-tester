7644b52aca9e7fe3afd52aa0dd414297b1f00e62
Jamie Scaife <10353998+jamieweb@users.noreply.github.com>
2018-07-29 15:43:01 +0100

    Sending argument to nodejs

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-node crawl.js
-sleep 1 #This sleep is required otherwise PHP will segfault when it is terminated at the end of the build.
+node crawl.js "${1}"
+sleep 3 #This sleep is required otherwise PHP will segfault when it is terminated at the end of the build.
 printf "\nCrawled Pages:\n$(cat crawled.csv)\n\n"
 if [ -s csp-reports.txt ]; then printf "Content Security Policy Reports:\n$(cat csp-reports.txt)"; else printf "No Reports Generated! Success!"; fi
